132410e70db399cb6f03676160270d73dab0bdfc
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2023-03-03 12:25:59 +0100

    inquirer added

--- build/index.js
+++ build/index.js
@@ -1,2 +1,15 @@
-"use strict";
-console.log('Init');
+import inquirer from 'inquirer';
+let playerName = "";
+async function askName() {
+    const answer = await inquirer.prompt({
+        name: "player_name",
+        type: "input",
+        message: "What is your name?",
+        default() {
+            return "Player";
+        }
+    });
+    playerName = answer.player_name;
+}
+await askName();
+console.log(`Hi ${playerName}!`);
